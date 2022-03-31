0646927f6503967ff875108bdc2c74f702ebec2f
LobedGaming <andradeortizobed@gmail.com>
2022-03-30 23:56:24 -0400

    first commit

---
+++ resources/views/components/boton.blade.php
@@ -0,0 +1,15 @@
+<!-- component -->
+<!-- follow me on twitter @asad_codes -->
+
+<div class="flex flex-wrap place-items-center h-screen">
+    <!-- buttons -->
+    <div class="text-center">
+        <button class="p-2 pl-5 pr-5  transform bg-indigo-500   text-lg rounded-lg focus:border-4 border-black-300">
+			<a href="{{ route('login') }}" class="text-sm text-gray-700 dark:text-black-500">INICIAR SECION</a>
+		</button>
+        <button class="p-2 pl-5 pr-5 bg-transparent border-2 border-indigo-500 text-black-500 text-lg rounded-lg transition-colors duration-700 transform hover:bg-indigo-500 hover:text-gray-100 focus:border-4 focus:border-indigo-300">
+			<a href="{{ route('register') }}" class="ml-4 text-sm text-gray-700 dark:text-black-500">REGISTRATE</a>
+		</button>
+    </div>
+</div>
+{{-- hover:bg-blue-400--}}
---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,21 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-bold text-xl text-gray-800 leading-tight">
+            {{-- {{ __('Dashboard') }} --}}
+
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+                {{-- <x-jet-welcome /> --}}
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia tempore ut pariatur nobis odit accusamus magnam nemo architecto vitae. Assumenda culpa ex saepe laboriosam quae nulla quas dolorum earum dolores.</p>
+                <p>Pariatur ad asperiores veniam nesciunt, tenetur adipisci vitae! Maiores earum impedit deserunt dignissimos magnam architecto nihil sequi ipsa libero, repellat fugit adipisci quibusdam repudiandae, temporibus recusandae, nobis provident eligendi aut.</p>
+                <p>Aliquam facilis ipsum est eos provident omnis amet pariatur ex repellendus esse unde et nisi commodi, error dolor eaque! Odit, id officiis sapiente alias distinctio harum facilis consectetur consequatur quibusdam!</p>
+                <p>Facere ullam magnam exercitationem deleniti ipsam earum iure reiciendis accusamus animi mollitia architecto necessitatibus ratione totam cupiditate similique, est ducimus repellat. Ipsa exercitationem officia architecto quidem dolor perspiciatis cumque ducimus?</p>
+                <p>Repudiandae totam officia sapiente vitae suscipit, fugit magnam ullam voluptatem autem. Neque magni voluptatibus nemo, delectus dignissimos natus maxime id deserunt fuga assumenda. Accusantium, ad. Porro eveniet perspiciatis distinctio reiciendis.</p>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
